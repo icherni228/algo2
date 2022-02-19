8154424f09ab2cba0503872c15dc79956ee524a8
icherni228 <91370544+icherni228@users.noreply.github.com>
2022-02-19 13:14:26 +0300

    Add files via upload

---
+++ Щедрый посититель.py
@@ -0,0 +1,9 @@
+summa = int(input("Сумма?"))
+summa10 = 0.1
+summa20 = 0.2
+summaall10 = summa*summa10
+summaall20 = summa*summa20
+summa101 = summa-summaall10
+summa201 = summa-summaall20
+print("Сумма - 10% - ", summa101)
+print("сумма - 20% - ", summa201)
